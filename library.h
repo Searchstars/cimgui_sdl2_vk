9662ca3743c6fed1d2bcc8c9b333229166b60f5e
Searchstars <searchstars@163.com>
2024-09-15 19:04:09 +0800

    init

---
+++ library.h
@@ -0,0 +1,10 @@
+#ifndef CIMGUI_SDL2_VK_LIBRARY_H
+#define CIMGUI_SDL2_VK_LIBRARY_H
+
+#include "cimgui.h"
+#include "cimgui_sdl2.h"
+#include "cimgui_vk.h"
+
+void hello();
+
+#endif //CIMGUI_SDL2_VK_LIBRARY_H
